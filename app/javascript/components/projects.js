0af359db24ce19b565f0a093e4f139fb626a24a3
Beatriz Longley <beatrizsousacosta@gmail.com>
2020-12-07 16:19:35 +0100

    home page done - content / projects with links

--- app/javascript/components/projects.js
+++ app/javascript/components/projects.js
@@ -1,28 +1,42 @@
 const projectImgInfo = () => {
-  const imageDiv = document.querySelectorAll('.project-img');
+  // Dining Room
+  const imageDivDr = document.querySelector('#project-img-dr');
 
-  // imageDiv.addEventListener('mouseenter', function() {
-  //   const info = document.querySelectorAll('.github-link');
-  //   info.classList.remove('hidden');
-  // });
+  imageDivDr.addEventListener('mouseenter', function() {
+    const infoDr = document.querySelector('#link-dr');
+    infoDr.classList.remove('hidden');
+  });
 
-  for (var i = 0; i < imageDiv.length; i++) {
-    imageDiv[i].addEventListener('mouseenter', function () {
-      const info = document.querySelectorAll('.github-link');
-      for (var i = 0; i < info.length; i++) {
-        info[i].classList.remove('hidden');
-      }
-    });
-  }
+   imageDivDr.addEventListener('mouseleave', function() {
+    const infoDr = document.querySelector('#link-dr');
+    infoDr.classList.add('hidden');
+  });
 
-  for (var i = 0; i < imageDiv.length; i++) {
-    imageDiv[i].addEventListener('mouseleave', function () {
-      const info = document.querySelectorAll('.github-link');
-      for (var i = 0; i < info.length; i++) {
-        info[i].classList.add('hidden');
-      }
-    });
-  }
+  // Teamup
+  const imageDivTu = document.querySelector('#project-img-tu');
+
+  imageDivTu.addEventListener('mouseenter', function() {
+    const infoTu = document.querySelector('#link-tu');
+    infoTu.classList.remove('hidden');
+  });
+
+   imageDivTu.addEventListener('mouseleave', function() {
+    const infoTu = document.querySelector('#link-tu');
+    infoTu.classList.add('hidden');
+  });
+
+  // PsychIO
+  const imageDivPi = document.querySelector('#project-img-pi');
+
+  imageDivPi.addEventListener('mouseenter', function() {
+    const infoPi = document.querySelector('#link-pi');
+    infoPi.classList.remove('hidden');
+  });
+
+   imageDivPi.addEventListener('mouseleave', function() {
+    const infoPi = document.querySelector('#link-pi');
+    infoPi.classList.add('hidden');
+  });
 }
 
 export { projectImgInfo };
